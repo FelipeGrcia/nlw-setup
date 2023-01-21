85ed9a22e1503804fd36046db6d138035da21312
felipegarcia <felipesilviagarcia1@gmail.com>
2023-01-21 17:19:08 -0300

    script

--- script.js
+++ script.js
@@ -3,33 +3,28 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('header button')
 
 button.addEventListener('click', add)
-form.addEventListener("change", save)
+form.addEventListener('change', save)
 
 
 function add(){
-  
-    const today = "01/01"
-
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
     const dayExists = nlwSetup.dayExists(today)
 
     if(dayExists){
         alert("Dia já incluso ❌ ")
         return
     }
+
     alert ('Dia adicionado com Sucesso ✔')
     nlwSetup.addDay(today)
 }
 
 function save(){
-    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+    localStorage.setItem("nlwSetup@habits", JSON.stringify(nlwSetup.data))
+
 }
 
 
-
-//const data = {
-//    run:['01-01', '01-02', '01-03', '01-04', '01-05', '01-06', '01-07', '01-08', '01-09', '01-10',],
-//    dog: ['01-03']
-//}
-const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {} 
+const data = JSON.parse(localStorage.getItem("nlwSetup@habits")) || {} 
 nlwSetup.setData(data)
 nlwSetup.load()
